0f983953884beefc5830864c6f25d2cb0e27e760
Christopher Sawala <chris.sawala@gmail.com>
2016-11-06 19:08:38 +0000

    added to handle response from example.org

--- day6/HTTP/https_example.js
+++ day6/HTTP/https_example.js
@@ -8,8 +8,9 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function(){
+var callback = function(response){
   console.log('In response to handler callback');
+  console.log('Response: ', response);
 };
 
 console.log("I'm about to make a request!");
