e9c967487e11c9a5989d8c21b1dbefdadd9d0cb1
Christopher Sawala <chris.sawala@gmail.com>
2016-11-06 19:03:06 +0000

    played with example.org request

--- day6/HTTP/https_example.js
+++ day6/HTTP/https_example.js
@@ -1,3 +1,19 @@
 var https = require('https');
 
-console.log("I dudditz!");
+// console.log("I dudditz!");
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+// called by https when the request is made.
+var callback = function(){
+  console.log('In response to handler callback');
+};
+
+console.log("I'm about to make a request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
