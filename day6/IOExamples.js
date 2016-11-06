0b3cde9e5d1a99ea741f2fedfc47a99ec460962f
Christopher Sawala <chris.sawala@gmail.com>
2016-11-06 16:58:14 +0000

    Add IO and stream examples as well as first stage HTTP client and streams

---
+++ day6/IOExamples.js
@@ -0,0 +1,28 @@
+/* ==============================================
+================== Synchronous ==================
+============================================== */
+
+var fs = require("fs");
+
+var filePath = "/tmp/test_sync.txt";
+var fileData = "Testing synchronous file write.\n";
+
+fs.writeFileSync(filePath, fileData);
+console.log("successfully wrote to", filePath);
+
+
+/* ==============================================
+================== Asynchronous =================
+============================================== */
+
+var fs = require("fs");
+
+var filePath = "/tmp/test_async.txt";
+var fileData = "Testing asynchronous file write.\n";
+
+fs.writeFile(filePath, fileData, function(err) {
+  if (err) {
+    throw err;
+  }
+  console.log("Successfully wrote to", filePath);
+});
